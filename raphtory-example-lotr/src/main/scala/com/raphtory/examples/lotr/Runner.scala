d8b4e13c08062a35ac2aa2269f85bae8e734aa35
Haaroon Y <Haaroon@users.noreply.github.com>
2021-11-18 15:18:32 +0000

    Update Runner.scala

--- raphtory-example-lotr/src/main/scala/com/raphtory/examples/lotr/Runner.scala
+++ raphtory-example-lotr/src/main/scala/com/raphtory/examples/lotr/Runner.scala
@@ -4,14 +4,14 @@ import com.raphtory.algorithms.{GraphState, TriangleCount}
 import com.raphtory.core.build.server.RaphtoryGraph
 import com.raphtory.spouts.FileSpout
 import com.raphtory.examples.lotr.graphbuilders.LOTRGraphBuilder
-import com.raphtory.examples.lotr.analysis.DegreesAnalyser
+import com.raphtory.examples.lotr.analysis.DegreesSeperation
 
 object Runner extends App{
         val source  = new FileSpout("src/main/scala/com/raphtory/examples/lotr/data/","lotr.csv")
         val builder = new LOTRGraphBuilder()
         val rg = RaphtoryGraph[String](source,builder)
 
-        rg.pointQuery(DegreesAnalyser(name = "Gandalf"),32670)
+        rg.pointQuery(DegreesSeperation(name = "Gandalf"),32670)
 }
         //rg.rangeQuery(new ConnectedComponents(Array()), serialiser = new DefaultSerialiser, start=1, end = 32674, increment=1000,windowBatch=List(10000, 1000,100))
         //rg.rangeQuery(ConnectedComponents(), serialiser = new DefaultSerialiser,start = 1,end = 32674,increment = 100,arguments)
